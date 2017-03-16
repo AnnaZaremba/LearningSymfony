d97219f73d46b808e3ac8f1b726638771063503a
Anna Zaremba <aniazarem@gmail.com>
2017-03-16 08:45:02 +0100

    New Form Validation

--- src/AppBundle/Controller/NewFormController.php
+++ src/AppBundle/Controller/NewFormController.php
@@ -4,10 +4,10 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Task;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
 class NewFormController extends Controller
 {
@@ -15,22 +15,40 @@ class NewFormController extends Controller
      * @Route("/newform", name="newform")
      */
 
-public function newAction(Request $request)
-{
-// create a task and give it some dummy data for this example
-$task = new Task();
-$task->setTask('Write a blog post');
-$task->setDueDate(new \DateTime('tomorrow'));
+    public function newAction(Request $request)
+    {
+        // just setup a fresh $task object (remove the dummy data)
+        $task = new Task();
 
-$form = $this->createFormBuilder($task)
-->add('task', TextType::class)
-->add('dueDate', DateType::class)
-->add('save', SubmitType::class, array('label' => 'Create Post'))
-->getForm();
+        $form = $this->createFormBuilder($task)
+            ->add('task', TextType::class)
+            ->add('dueDate', DateType::class)
+            ->add('save', SubmitType::class, array('label' => 'Create Task'))
+            ->add('saveAndAdd', SubmitType::class, array('label' => 'Save and Add'))
+            ->getForm();
 
-return $this->render('default/new.html.twig', array(
-'form' => $form->createView(),
-));
-}
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            // wykonanie jakiegoś działania, jak zapis zadania do bazy danych
+
+            $nextAction = $form->get('saveAndAdd')->isClicked()
+                ? 'task_new'
+                : 'task_success';
+
+            return $this->redirect($this->generateUrl($nextAction));
+        }
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // ... perform some action, such as saving the task to the database
+
+            return $this->redirectToRoute('task_success');
+        }
+
+        return $this->render('default/new.html.twig', array(
+            'form' => $form->createView(),
+        ));
+
+    }
 }
 
--- src/AppBundle/Entity/Task.php
+++ src/AppBundle/Entity/Task.php
@@ -1,10 +1,20 @@
 <?php
 namespace AppBundle\Entity;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Task
 {
-protected $task;
-protected $dueDate;
+    /**
+     * @Assert\NotBlank()
+     */
+    public $task;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Type("\DateTime")
+     */
+    protected $dueDate;
 
 public function getTask()
 {
