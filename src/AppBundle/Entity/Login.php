c93300ddc873bb922a69fec142ac3896bdc234ce
Anna Zaremba <aniazarem@gmail.com>
2017-03-31 19:25:20 +0200

    #14 Ksiazka kucharska - panel logowania

---
+++ src/AppBundle/Entity/Login.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="ksiazkakucharskalogowanie")
+ */
+class Login
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=55)
+     */
+    protected $nazwa;
+
+    /**
+     * @ORM\Column(type="string", length=12)
+     */
+    protected $haslo;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNazwa()
+    {
+        return $this->nazwa;
+    }
+
+    /**
+     * @param mixed $nazwa
+     */
+    public function setNazwa($nazwa)
+    {
+        $this->nazwa = $nazwa;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHaslo()
+    {
+        return $this->haslo;
+    }
+
+    /**
+     * @param mixed $haslo
+     */
+    public function setHaslo($haslo)
+    {
+        $this->haslo = $haslo;
+    }
+}
